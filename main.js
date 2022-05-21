5df6c54bf2acdd6b5d4da45523350bcb00594e09
neil dela cruz <genoznightmare@gmail.com>
2022-05-21 23:09:20 +0800

    initial state of loop initiation. need to auto update the list

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const themeProviderBtn = document.getElementById("themeProvider");
 const body = document.querySelector("body");
 const header = document.querySelector("header");
+
+const toDoLists = document.getElementById("toDoLists");
+const todoListBracket = document.getElementById("odoListBracket");
+
+//variable for inputs
+const inputNewToDo = document.getElementById("inputNewToDo");
+
 /*themprovider */
-console.log(body.dataset.theme);
 themeProviderBtn.addEventListener("click", themeProviderFunc);
 
 function themeProviderFunc() {
@@ -13,4 +19,99 @@ function themeProviderFunc() {
     themeProviderBtn.setAttribute("src", "images/icon-moon.svg");
     body.setAttribute("data-theme", "darkTheme");
   }
+}
+
+/*input to add to do list */
+inputNewToDo.addEventListener("keypress", addNewToDo);
+
+function addNewToDo(event) {
+  if (inputNewToDo.value !== "" && event.key === "Enter") {
+    let NEW_TODO_VALUE = inputNewToDo.value.toLowerCase();
+
+    if (localStorage.getItem("data") == null) {
+      localStorage.setItem("data", "[]");
+    }
+
+    let old_todo = JSON.parse(localStorage.getItem("data"));
+    old_todo.push(NEW_TODO_VALUE);
+    localStorage.setItem("data", JSON.stringify(old_todo));
+    inputNewToDo.value = "";
+  }
+}
+
+let LOCAL_STORAGE = JSON.parse(localStorage.getItem("data"));
+console.log(LOCAL_STORAGE);
+// LOCAL_STORAGE.forEach(addToLists);
+
+// function addToLists(currentValue, index) {
+//   console.log(currentValue, index);
+//   let htmlElement = `<div class="to-do-list" id="todoListBracket">
+//   <div class="checkbox-container">
+//     <div class="checkbox">
+//       <img
+//         src="images/icon-check.svg"
+//         alt="checkbox button"
+//         id="checkboxBtn"
+//       />
+//     </div>
+//   </div>
+//   <div class="to-do-text">${currentValue}${index}</div>
+//   <div class="close-button-container" id="closeBtn">
+//     <div class="close-button">
+//       <img src="images/icon-cross.svg" alt="close button" />
+//     </div>
+//   </div>
+// </div>`;
+
+//   toDoLists.innerHTML = htmlElement;
+// }
+function loop() {
+  let htmlElement = "<div>";
+  for (let i = 0; i < LOCAL_STORAGE.length; i++) {
+    htmlElement += `<div class="to-do-list" id="todoListBracket">
+    <div class="checkbox-container">
+      <div class="checkbox">
+        <img
+          src="images/icon-check.svg"
+          alt="checkbox button"
+          id="checkboxBtn"
+        />
+      </div>
+    </div>
+    <div class="to-do-text">${LOCAL_STORAGE[i]}</div>
+    <div class="close-button-container closeBtn" data-close-btn="closeBtn">
+      <div class="close-button">
+        <img src="images/icon-cross.svg" alt="close button" />
+      </div>
+    </div>
+  </div>
+  `;
+  }
+  htmlElement += `<div class="to-do-lists-filters">
+    <p class="to-do-items-left m-0 item-filter">5 items left</p>
+    <div class="to-do-filter-options-desktop">
+      <div class="to-do-filter-all filter">All</div>
+      <div class="to-do-filter-active filter">Active</div>
+      <div class="to-do-filter-all filter">Completed</div>
+    </div>
+    <div class="to-do-filter-clear-completed item-filter">
+      Clear Completed
+    </div>
+  </div>
+  </div>`;
+  htmlElement += "</div>";
+  toDoLists.innerHTML = htmlElement;
+}
+
+loop();
+
+/*closeBtn function */
+const checkboxBtn = document.getElementById("checkboxBtn");
+const closeBtn = document.querySelector(".closeBtn");
+closeBtn.addEventListener("click", crossOutFunction);
+
+function crossOutFunction() {
+  this.parentElement.style.display = "none";
+  console.log(this.parentElement);
+  localStorage.removeItem("data");
 }
--- plan.md
+++ plan.md
@@ -1,1 +1,18 @@
 night mode first to create
+
+//input to add to do list
+when input it must add to the lists and pops to the top of the to dolists
+
+//add list to completed
+if click then add to completed but not stay to all filter and also check underlined text
+
+//delete list
+if the close button is clicked then it must delete from all and update the array set of to do list
+
+//active list
+//all active list is filtered (those all that unchecked)
+
+//clear completed
+if clear then it ask if the user is sure to delete and then when the prompt pops up it triggers event that delete the completed lists
+
+//the item left indicator counts all the active in array
